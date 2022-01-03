7bf10a1fb0c5ec9e160964e4b91c1fc3a9abd1e4
Mate <md.quintero@uniandes.edu.co>
2022-01-03 11:42:23 -0500

    page2 appbar

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -5,6 +5,9 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 2'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
